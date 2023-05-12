d737c21ac1b46856df086f7c0c0592db7729922b
adamczein <112838729+adamczein@users.noreply.github.com>
2023-05-11 23:53:27 -0400

    question 4 et 5 fonctionne

--- main.cpp
+++ main.cpp
@@ -178,13 +178,113 @@ void q3()
 
 }
 
+void q4() {
+	const int taille = 10; //not sure si il fait ou pas mettre const
+	int tab[taille];
+
+	// Saisie du tableau d'entiers
+	cout << "Entrez 10 nombres entiers : ";
+	for (int i = 0; i < taille; i++) {
+		cin >> tab[i]; //pour ajouter les nombres entrees dans le tableau
+	}
+
+	// Tri du tableau d'entiers
+	for (int i = 0; i < taille - 1; i++) {
+		int minIndex = i;
+		for (int j = i + 1; j < taille; j++) {
+			if (tab[j] < tab[minIndex]) {
+				minIndex = j;
+			}
+		}
+		int temp = tab[i];
+		tab[i] = tab[minIndex];
+		tab[minIndex] = temp;
+	}
+
+	// Affichage du tableau trié
+	cout << "Voici le tableau trié : ";
+	for (int i = 0; i < taille; i++) {
+		cout << tab[i] << " ";
+	}
+	cout << endl;
+
+}
+
+//les constantes necessiares pour question 5
+const int MAX_ALIMENTS = 10;
+
+struct Aliment {
+	string nom;
+	string type;
+	int quantite;
+	double prix;
+};
+
+//la question 5 peut supporter jusqu'a 10 aliments  (limite imposer dans la question)
+void q5() {
+	// Création du tableau d'aliments
+	Aliment inventaire[MAX_ALIMENTS];
+	int nbAliments = 0;
+
+	// Lecture du fichier d'inventaire
+	ifstream fichier("inventaire.txt");
+	if (!fichier) {
+		cout << "Erreur: impossible d'ouvrir le fichier d'inventaire." << endl;
+		return;
+	}
+
+	while (nbAliments < MAX_ALIMENTS && !fichier.eof()) {
+		string ligne;
+		getline(fichier, ligne);
+		if (!ligne.empty()) {
+			size_t pos1 = ligne.find('\t');
+			size_t pos2 = ligne.find('\t', pos1 + 1);
+			size_t pos3 = ligne.find('\t', pos2 + 1);
+			string nom = ligne.substr(0, pos1);
+			string type = ligne.substr(pos1 + 1, pos2 - pos1 - 1);
+			int quantite = stoi(ligne.substr(pos2 + 1, pos3 - pos2 - 1));
+			double prix = stod(ligne.substr(pos3 + 1));
+			Aliment aliment = { nom, type, quantite, prix };
+			inventaire[nbAliments++] = aliment;
+		}
+	}
+
+	fichier.close();
+
+	// Recherche de l'aliment le plus cher
+	int indexMaxPrix = 0;
+	double maxPrix = inventaire[0].prix;
+
+	for (int i = 1; i < nbAliments; i++) {
+		if (inventaire[i].prix > maxPrix) {
+			indexMaxPrix = i;
+			maxPrix = inventaire[i].prix;
+		}
+	}
+
+	// Affichage de tous les aliments de l'inventaire
+	for (int i = 0; i < nbAliments; i++) {
+		cout << inventaire[i].quantite << " " << inventaire[i].nom << " ("
+			<< inventaire[i].type << ") a " << inventaire[i].prix
+			<< "$ chacun" << endl;
+	}
+
+	// Affichage de l'aliment le plus cher
+	cout << endl << "L'aliment le plus cher est : " << inventaire[indexMaxPrix].quantite
+		<< " " << inventaire[indexMaxPrix].nom << " (" << inventaire[indexMaxPrix].type
+		<< ") a " << inventaire[indexMaxPrix].prix << "$ chacun" << endl;
+}
+
+
 
 
 int main()
 {
-	q1();
+	/*q1();
 	q2();
-	q3();
+	q3();*/
+	/*q4();*/
+	q5();
 	return 0;
 }
 
